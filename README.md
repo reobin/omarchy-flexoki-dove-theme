5976ed2ab3128f62b2bf9962a372058d1ae7b20d
Robin Gagnon <me@reobin.dev>
2026-03-10 22:05:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# `omarchy-flexoki-dove-theme`
+
+## Installation
+
+```bash
+omarchy-theme-install https://github.com/reobin/omarchy-flexoki-dove-theme.git
+```
---
+++ neovim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "reobin/flexoki-dove",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki-dove",
+    },
+  },
+}
